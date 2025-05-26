fe7009d50f9eea42925910be1675b1f7518d99a4
Victor Seleimend <sseleimend@gmail.com>
2025-05-26 19:43:47 -0300

    Remove items from the facts list

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
+
+factsList.innerHTML = "";
 
 btn.addEventListener("click", () => {
   form.classList.toggle("hidden");
