9ea3201cf492e4ec2feff735c0460fde101b41fd
Victor Seleimend <sseleimend@gmail.com>
2025-05-27 10:33:31 -0300

    Update App.jsx to implement the application skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
