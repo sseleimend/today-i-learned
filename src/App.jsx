7096f9853ec09df08fa498b847c67e2e4b35b6dd
Victor Seleimend <sseleimend@gmail.com>
2025-05-27 16:08:54 -0300

    Add disputed flag

--- src/App.jsx
+++ src/App.jsx
@@ -217,6 +217,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -236,6 +238,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed && <span className="disputed">[⛔️DISPUTED]</span>}{" "}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">
           (Source)
