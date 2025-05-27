950f072f6156ef09a99d94999ebbcd08781cb9d8
Victor Seleimend <sseleimend@gmail.com>
2025-05-27 11:40:09 -0300

    Add new fact form content

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,20 @@ function Header({ setShowForm, showForm }) {
 }
 
 function NewFactForm() {
-  return <form className="fact-form">Fact form</form>;
+  return (
+    <form className="fact-form hidden">
+      <input type="text" placeholder="Share a fact with the world..." />
+      <span>200</span>
+      <input type="text" placeholder="Trustworthy source" />
+      <select>
+        <option value="">Choose category</option>
+        <option value="technology">Technology</option>
+        <option value="science">Science</option>
+        <option alue="finance">Finance</option>
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 const CATEGORIES = [
